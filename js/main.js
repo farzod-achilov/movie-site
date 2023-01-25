cd2c7f89f3874d2a2925fe4254eab7aec77bef8f
Farzod <107624021+farzod-achilov@users.noreply.github.com>
2023-01-25 12:50:53 +0500

    movie

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ const IMG_URL = `http://img.omdbapi.com/?apikey=${API_KEY}`;
 
 const elForm = document.querySelector("[data-movie-form]");
 const elList = document.querySelector("[data-movie-list]");
+const elPagination = document.querySelector("[data-modal-pagination]");
 const elModal = document.querySelector("[data-movie-modal-open]");
-const elModalContent = document.querySelector("[data-movie-content]")
+const elModalContent = document.querySelector("[data-movie-content]");
 
 elForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
@@ -20,28 +21,45 @@ async function searchMovies(quary) {
   const searchResult = await res.json();
 
   renderMovies(searchResult.Search);
+  renderPaginetion(Math.ceil(+searchResult.totalResults / 10));
+}
+
+async function getMovie(movieId) {
+  const res = await fetch(`${API_URL}&i=${movieId}`);
+
+  return await res.json();
 }
 
 function renderMovies(movies) {
   elList.innerHTML = "";
   let html = "";
   movies.forEach((movie) => {
-    html += `<div class="cinema"><img class="cinema__img" width="50" src="${movie.Poster}" alt="${movie.Title}"/>
+    html += `<div class="cinema"><img class="cinema__img"   height= "440px" width="50" src="${movie.Poster}" alt="${movie.Title}"/>
     <div class="d-flex align-items-center justify-content-between">
     <h2>${movie.Title}-${movie.Year} </h2>
     <button type="button" data-movie-modal-open="#test-modal" data-movie-id="${movie.imdbID}" class="btn cinema__btn"><img class="cinema__btn-img" src="./img/more.svg" alt="more"></button>
     </div>
         </div>`;
-
-    // elList.append(fillModal(movie));
   });
 
   elList.innerHTML = html;
+}
+
+function renderPaginetion(totalPages) {
+  elPagination.innerHTML = "";
+  let html = "";
+
+  for (let i = 1; i <= totalPages; i++) {
+    html += `<li class="page-item"><a class="page-link" data-movie-pages=${i} href="?page=${i}">${i}</a></li>`;
+  }
+
+  elPagination.innerHTML = html;
 }
 document.addEventListener("click", (evt) => {
   onOpenModal(evt);
   onCloseOutsideClick(evt);
   onModalCloseClick(evt);
+  onPageClick(evt);
 });
 
 function onOpenModal(evt) {
@@ -50,14 +68,18 @@ function onOpenModal(evt) {
   if (!el) return;
 
   const modalSel = el.dataset.movieModalOpen;
-  const movieId = el.dataset.movieId
+  const movieId = el.dataset.movieId;
+  const elModal = document.querySelector(modalSel);
 
-  fillModal(movieId)
-  document.querySelector(modalSel).classList.add("show");
+  const elModalSpinner = elModal.querySelector("[data-modal-spinner]");
+
+  fillModal(movieId, elModalSpinner);
+
+  elModal.classList.add("show");
 }
 
 function onCloseOutsideClick(evt) {
-  const el = evt.target
+  const el = evt.target;
 
   if (!el.matches("[data-movie-modal]")) return;
 
@@ -72,18 +94,30 @@ function onModalCloseClick(evt) {
   el.parentElement.parentElement.classList.remove("show");
 }
 
-async function fillModal(movieId) {
+function onPageClick(evt) {
+  const el = evt.target.closest("[data-movie-page]");
+
+  if (!el) return;
+  evt.preventDefault();
+}
+
+async function fillModal(movieId, elModalSpinner) {
+  elModalSpinner.classList.remove("d-none");
   const movie = await getMovie(movieId);
 
- elModalContent.querySelector("[data-title]").textContent = movie.Title;
- elModalContent.querySelector("[data-year]").textContent = movie.Year;
- elModalContent.querySelector("[data-rated]").textContent = movie.Rated;
- elModalContent.querySelector("[data-released]").textContent = movie.Released;
- elModalContent.querySelector("[data-runtime]").textContent = movie.Runtime;
- elModalContent.querySelector("[data-genre]").textContent = movie.Genre;
- elModalContent.querySelector("[data-director]").textContent = movie.Director;
- elModalContent.querySelector("[data-metascore]").textContent = movie.Metascore;
- elModalContent.querySelector("[data-imdbrating]").textContent = movie.imdbrating;
- elModalContent.querySelector("[data-type]").textContent = movie.Type;
- elModalContent.querySelector("[data-id]").textContent = movie.imdbID;
+  elModalContent.querySelector("[data-title]").textContent = movie.Title;
+  elModalContent.querySelector("[data-year]").textContent = movie.Year;
+  elModalContent.querySelector("[data-rated]").textContent = movie.Rated;
+  elModalContent.querySelector("[data-released]").textContent = movie.Released;
+  elModalContent.querySelector("[data-runtime]").textContent = movie.Runtime;
+  elModalContent.querySelector("[data-genre]").textContent = movie.Genre;
+  elModalContent.querySelector("[data-director]").textContent = movie.Director;
+  elModalContent.querySelector("[data-imdbrating]").textContent =
+    movie.imdbrating;
+  elModalContent.querySelector("[data-type]").textContent = movie.Type;
+  elModalContent.querySelector("[data-id]").textContent = movie.imdbID;
+
+  elModalSpinner.classList.add("d-none");
 }
+
+// 40:00
